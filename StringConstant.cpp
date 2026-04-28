5f3746792203fdd6a33e80408ce8ab2c1220e633
bsc-com-31-23 <bsc-com-31-23@unima.ac.mw>
2026-04-28 12:28:35 +0200

    Add files via upload

---
+++ StringConstant.cpp
@@ -0,0 +1,21 @@
+/*#include <iostream>
+using namespace std;
+
+int main() {
+    char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+
+    cout << "Greeting message: ";
+    cout << greeting << endl;
+    
+    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    string name=" mirriam ";
+    for(int i=0;i<name.length();i++){
+    cout<<name[i];
+    }
+    return 0;
+}
